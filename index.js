eb89aa5682dc836b4e116cfb34ebf95fde9a7efc
Pritam <pritamgujar27@gmail.com>
2024-03-20 19:46:36 +0530

    Adding all CRUD operations

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+
+app.use(bodyParser.urlencoded());
 
 app.get("/", (req, res) => {
   res.send("1st express server");
@@ -15,11 +17,11 @@ const User = mongoose.model("User", {
   class: Number,
 });
 
-// const Child = mongoose.model("Child", {
-//   firstName: String,
-//   lastName: String,
-//   class: Number,
-// });
+const Child = mongoose.model("Child", {
+  firstName: String,
+  lastName: String,
+  class: Number,
+});
 
 // get request
 app.get("/users", async (req, res) => {
@@ -39,7 +41,7 @@ app.get("/users", async (req, res) => {
 app.get("/users/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const users = await User.find();
+    const users = await User.findById(id);
     res.json({
       data: users,
     });
@@ -52,13 +54,101 @@ app.get("/users/:id", async (req, res) => {
 
 // post request
 app.post("/users", async (req, res) => {
+  const { firstName, lastName, classNumber } = req.body;
   try {
-    const users = await User.find();
+    await User.create({
+      firstName: firstName,
+      lastName: lastName,
+      class: classNumber,
+    });
     res.json({
-      data: users,
+      status: "SUCCESS",
     });
   } catch (error) {
     res.json({
+      message: "Something went wrong",
+    });
+  }
+});
+
+// patch method
+app.patch("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, classNumber } = req.body;
+  try {
+    await User.findByIdAndUpdate(id, {
+      firstName, //
+      lastName, //
+      class: classNumber,
+    });
+    res.json({
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
+
+// app.put("/users/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const { firstName, lastName, classNumber } = req.query;
+//   console.log(req.query);
+//   try {
+//     await User.findByIdAndUpdate(id, req.query);
+//     res.json({
+//       status: "SUCCESS",
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Something went wrong",
+//     });
+//   }
+// });
+
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await User.findByIdAndDelete(id);
+    res.json({
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
+
+// search with specific details
+app.get("/users", async (req, res) => {
+  try {
+    // const { firstName, lastName, classNumber } = req.query;
+
+    // console.log(firstName + " " + lastName + " " + classNumber);
+
+    // console.log(typeof classNumber);
+
+    console.log(req.query);
+    // const query = {};
+    // if (firstName) {
+    //   query.firstName = firstName;
+    // }
+    // if (lastName) {
+    //   query.lastName = lastName;
+    // }
+    // if (classNumber) {
+    //   query.class = classNumber;
+    // }
+
+    const users = await User.find(req.query);
+    res.json({
+      status: "SUCCESS",
+      data: users,
+    });
+  } catch (error) {
+    res.status(500).json({
       message: "Something went wrong",
     });
   }
